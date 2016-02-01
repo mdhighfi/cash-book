1af919475428bd75a9e1768ab12a0294d060243f
mdhighfi <matthew.highfield@gmail.com>
2016-01-31 17:01:48 -0800

    Add methods to users controller

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,2 +1,21 @@
 class UsersController < ApplicationController
+  def new
+    @user = User.new
+  end
+
+  def create
+    @user = User.new(user_params)
+    if @user.save
+      log_in!(@user)
+      redirect_to root_url
+    else
+      flash[:errors] = @user.errors.full_messages
+      redirect_to new_user_url
+    end
+  end
+
+  private
+  def user_params
+    params.require(:user).permit(:username, :password)
+  end
 end
